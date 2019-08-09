42a5f9843740b82d2a7d56aade5d178254dfd9b3
gear <33369211+iRequestUResponse@users.noreply.github.com>
2019-08-09 16:16:56 +0900

    cookie  - java cookie 문자열 파싱  - login.jsp rememberMe 기능 추가

---
+++ src/test/java/kr/or/ddit/util/CookieUtilTest.java
@@ -0,0 +1,34 @@
+package kr.or.ddit.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CookieUtilTest {
+
+	/**
+	* Method : getCookieTest
+	* 작성자 : PC-17
+	* 변경이력 :
+	* Method 설명 : 쿠키 가져오기 테스트
+	*/
+	@Test
+	public void getCookieTest() {
+		/***Given***/
+		String cookieString = "userId=brown; rememberMe=Y; test=testValue";
+
+		/***When***/
+		String userIdCookieValue = CookieUtil.getCookie(cookieString, "userId");
+		String rememberMeCookieValue = CookieUtil.getCookie(cookieString, "rememberMe");
+		String testCookieValue = CookieUtil.getCookie(cookieString, "test");
+		String notExistsCookieValue = CookieUtil.getCookie(cookieString, "notExists");
+
+		/***Then***/
+		assertEquals("brown", userIdCookieValue);
+		assertEquals("Y", rememberMeCookieValue);
+		assertEquals("testValue", testCookieValue);
+		assertEquals(null, notExistsCookieValue);
+		assertNull(notExistsCookieValue);
+	}
+
+}
