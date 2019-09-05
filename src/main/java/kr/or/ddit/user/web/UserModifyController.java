fee18d6cdb4c53f92fb01c3f29fb9c31a66ee27c
gear <33369211+iRequestUResponse@users.noreply.github.com>
2019-09-05 15:15:39 +0900

    파라미터 임의로 수정하기  HttpServletRequestWrapper 상속   . parameterMap request 객체의 파라미터맵을 바탕으로 신규 생성   . 파라미터 관련 메소드 4가지를  재정의     . getParameter     . getParameterValues     . getParameterMap     . getParameterNames   . filter에서 메소드 인자로 받은 request 객체 대신에     wrapper 인스턴스를 생성하여 doFilter 메소드에 인자로 전달(request -> wrapper)
    CharacterEncodingFilter 적용
     . servlet에 있는 request.setCharacterEncoding("UTF-8") 호출 부분을
       filter로 이관, 코드를 한 곳에서만 호출되도록 수정

--- src/main/java/kr/or/ddit/user/web/UserModifyController.java
+++ src/main/java/kr/or/ddit/user/web/UserModifyController.java
@@ -45,7 +45,7 @@ public class UserModifyController extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
+//		request.setCharacterEncoding("UTF-8");
 		
 		
 		String userId = request.getParameter("userId");
